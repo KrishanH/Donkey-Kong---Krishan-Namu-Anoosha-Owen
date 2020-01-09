6b824887d3c05ace594824e5ddbd15657709f40a
owen-112 <58954260+owen-112@users.noreply.github.com>
2020-01-09 13:58:47 -0500

    Added Coins

---
+++ Donkey Kong/Coin50.java
@@ -0,0 +1,18 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Coin50 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Coin50 extends Actor
+{
+       
+    public void act() 
+    {
+        GreenfootImage image = getImage();  
+       image.scale(30, 30);
+        setImage(image);
+    }    
+}
